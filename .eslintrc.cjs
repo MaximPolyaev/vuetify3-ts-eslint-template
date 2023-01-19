1ecf5adcc51446e51639480cc518a849bea03fca
polyaevma <polyaevma@cloud-team.ru>
2023-01-19 16:39:42 +0300

    add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': isProduction ? ['warn', { allow: ['error', 'warn'] }] : 'off',
+    'no-debugger': isProduction ? 'error' : 'off',
+  },
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const t = 'test'
+console.log(t)
 defineProps<{
   msg: string
 }>()
@@ -10,7 +12,8 @@ defineProps<{
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      What's next?
     </h3>
   </div>
 </template>
