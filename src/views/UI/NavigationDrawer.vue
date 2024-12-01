f21b2dba5bf3b76298d7ff5afb7a3256ce393e63
mpolyaev <polyaev.maks@ya.ru>
2024-12-01 17:09:44 +0300

    upd navigation

--- src/views/UI/NavigationDrawer.vue
+++ src/views/UI/NavigationDrawer.vue
@@ -1,17 +1,18 @@
 <template>
   <v-navigation-drawer v-model="drawer">
-    <v-list-item>
-      <v-list-item-title class="text-h4 py-2">
-        Your app
-      </v-list-item-title>
+    <v-list-item @click="$router.push({ name: 'main' })">
+      <v-list-item-title class="text-h4 py-2">Your app</v-list-item-title>
     </v-list-item>
     <v-divider />
-    <v-list-item link title="Main" href="/" :active="$route.name === 'main'" />
     <v-list-item
-      link
+      title="Main"
+      :active="$route.name === 'main'"
+      @click="$router.push({ name: 'main' })"
+    />
+    <v-list-item
       title="Form"
-      href="/form"
       :active="$route.name === 'form'"
+      @click="$router.push({ name: 'form' })"
     />
   </v-navigation-drawer>
 </template>
